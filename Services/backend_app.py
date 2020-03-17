a20db4325702022842ad82bb5e1c3be9544edd1d
PMetcalf <paulmetcalf@hotmail.com>
2020-03-17 11:04:17 +0000

    Installed azure models to dev environment; Added pseudocode to backend file.

--- Services/backend_app.py
+++ Services/backend_app.py
@@ -5,3 +5,23 @@ Backend Application
 Application simulates backend, on-cloud service used to update simulated device.
 
 '''
+
+# Module importations
+from azure.iot.hub import IoTHubRegistryManager
+from azure.iot.hub.models import CloudToDeviceMethod, CloudToDeviceMethodResult
+from builtins import input
+import sys
+
+# Service connection string used to authenticate with the device hub.
+CONNECTION_STRING = ""
+DEVICE_ID = ""
+
+# Details of Direct Method to call.
+METHOD_NAME = "SetTelemetryInterval"
+METHOD_PAYLOAD = "10"
+
+# Routine updates telemetry interval via hub
+def iot_device_updatetelemetry_interval():
+    pass
+
+# Main method calls routine to update telemetry interval
