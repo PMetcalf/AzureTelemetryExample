68936dfa8cc0742c054d522e64b02ddbd5d88c68
PMetcalf <paulmetcalf@hotmail.com>
2020-03-17 11:29:41 +0000

    Added body to update telemetry method.

--- Services/backend_app.py
+++ Services/backend_app.py
@@ -13,15 +13,45 @@ from builtins import input
 import sys
 
 # Service connection string used to authenticate with the device hub.
-CONNECTION_STRING = "HostName=AzureTelemetryExampleHub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=glElhyl7qcPnjnBZYUTQob98kdzZRa97svoJO3c/0RQ="
-DEVICE_ID = "simulatedDevice"
+connection_string = "HostName=AzureTelemetryExampleHub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=glElhyl7qcPnjnBZYUTQob98kdzZRa97svoJO3c/0RQ="
+device_id = "simulatedDevice"
 
 # Details of Direct Method to call.
-METHOD_NAME = "SetTelemetryInterval"
-METHOD_PAYLOAD = "10"
+method_name = "SetTelemetryInterval"
+method_payload = "10"
 
 # Routine updates telemetry interval via hub
 def iot_device_updatetelemetry_interval():
-    pass
+    
+    # Try-except in use as webservice may fail
+    try:
+
+        # Create registry manager
+        registry_manager = IoTHubRegistryManager(connection_string)
+
+        # Call the direct method
+        device_method = CloudToDeviceMethod(method_name = method_name, 
+                                            payload = method_payload)
+        device_response = registry_manager.invoke_device_method(device_id, device_method)
+
+        # Provide feedback to user
+        print("")
+        print("Device Method Called")
+        print("Device Method Name     : {0}".format(method_name))
+        print("Device Method Payload  : {0}".format(method_payload))
+        
+        print("Device Response")
+        print("Response Status        : {0}".format(device_response.status))
+        print("Response Payload        : {0}".format(device_response.payload))
+
+        # Allow user to close service connection
+        input("Press Enter to continue ...\n")
+
+    except Exception as ex:
+        print("Unexpected error")
+        return
+
+    except KeyboardInterrupt:
+        print("Device Instruction stopped")
 
 # Main method calls routine to update telemetry interval
